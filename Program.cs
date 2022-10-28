1ee048dc63bc83086b2071b81ca7395b4d788988
Mr Simon C <iamontheinternet@yahoo.com>
2022-10-28 19:44:46 +0000

    add minimal api

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
 using Microsoft.Playwright;
 
-using var playwright = await Playwright.CreateAsync();
-await using var browser = await playwright.Chromium.LaunchAsync();
-var page = await browser.NewPageAsync();
-await page.GotoAsync("https://playwright.dev/dotnet");
-await page.ScreenshotAsync(new()
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", async () => await RunPlaywrightAsync());
+
+app.Run();
+
+async Task<string> RunPlaywrightAsync()
 {
-    Path = "screenshot.png"
-});
-Console.WriteLine("All working.");
+    using var playwright = await Playwright.CreateAsync();
+    await using var browser = await playwright.Chromium.LaunchAsync();
+    var page = await browser.NewPageAsync();
+    await page.GotoAsync("https://playwright.dev/dotnet");
+    await page.ScreenshotAsync(new()
+    {
+        Path = "screenshot.png"
+    });
+    Console.WriteLine("All working.");
+    return("All working");
+}
